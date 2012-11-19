de193a0b644f6907d0d264ecc4cad5e1f8e44991
Jarek Sacha <jsacha@users.sf.net>
2012-11-18 20:56:18 -0500

    Add explicit return types to methods and correct scaladoc  style.

--- src/sfxext/scene/control/MenuButton.scala
+++ src/sfxext/scene/control/MenuButton.scala
@@ -1,6 +1,9 @@
 package sfxext.scene.control
 
+import javafx.beans.{property => jfxbp}
 import javafx.scene.{control => jfxsc}
+import javafx.{collections => jfxc}
+import javafx.{geometry => jfxg}
 import scala.collection.JavaConversions.asJavaCollection
 import scalafx.Includes._
 import scalafx.geometry.Side
@@ -14,54 +17,41 @@ object MenuButton {
   implicit def sfxToggleButton2jfx(mb: MenuButton) = mb.delegate
 }
 
-
+/** Wrapper for `javafx.scene.control.MenuButton`. */
 class MenuButton(override val delegate: jfxsc.MenuButton = new jfxsc.MenuButton)
   extends ButtonBase(delegate)
   with SFXDelegate[jfxsc.MenuButton] {
 
-  /**
-   * Creates a toggle button with the specified text as its label.
-   */
+  /** Creates a toggle button with the specified text as its label. */
   def this(text: String) = this(new jfxsc.MenuButton(text))
 
-  /**
-   * Creates a toggle button with the specified text and icon for its label.
-   */
+  /** Creates a toggle button with the specified text and icon for its label. */
   def this(text: String, graphic: Node) = this(new jfxsc.MenuButton(text, graphic))
 
-  /**
-   * The items to show within this buttons menu.
-   */
-  def items = delegate.getItems
+  /** The items to show within this buttons menu. */
+  def items: jfxc.ObservableList[jfxsc.MenuItem] = delegate.getItems
 
-  def items_=(v: Iterable[MenuItem]) = {
-    items.addAll(v.map(_.delegate))
-  }
+  def items_=(v: Iterable[MenuItem]): Boolean = items.addAll(v.map(_.delegate))
 
-  /**
-   * Indicates on which side the ContextMenu should open in relation to the MenuButton.
-   */
-  def popupSide = delegate.popupSideProperty()
+
+  /** Indicates on which side the ContextMenu should open in relation to the MenuButton. */
+  def popupSide: jfxbp.ObjectProperty[jfxg.Side] = delegate.popupSideProperty()
 
   def popupSide_=(side: Side) {
     popupSide() = side
   }
 
-  /**
-   * Hides the ContextMenu if it was previously showing, and any showing submenus.
-   */
+  /** Hides the ContextMenu. */
   def hide() {
     delegate.hide()
   }
 
-  /**
-   * Gets the value of the property showing.
-   */
-  def showing = delegate.isShowing
+  /** Gets the value of the property showing. */
+  def showing: Boolean = delegate.isShowing
 
-  /**
-   * If the Menu is not disabled and the ContextMenu is not already showing, then this will cause the ContextMenu to be shown.
-   */
+  /** If the Menu is not disabled and the ContextMenu is not already showing,
+    * then this will cause the ContextMenu to be shown.
+    */
   def show() {
     delegate.show()
   }
--- src/sfxext/scene/control/SplitMenuButton.scala
+++ src/sfxext/scene/control/SplitMenuButton.scala
@@ -8,7 +8,7 @@ object SplitMenuButton {
   implicit def sfxSplitMenuButton2jfx(smb: SplitMenuButton) = smb.delegate
 }
 
-
+/** Wrapper for `javafx.scene.control.SplitMenuButton`. */
 class SplitMenuButton(override val delegate: jfxsc.SplitMenuButton = new jfxsc.SplitMenuButton)
   extends MenuButton(delegate)
   with SFXDelegate[jfxsc.SplitMenuButton]
