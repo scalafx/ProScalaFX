9df923998d59e0cf6914f9b97e1ae6a98dadefa4
Jarek Sacha <jpsacha@gmail.com>
2021-04-20 21:11:05 -0400

    Make FXMLAdoptionForm to run with Scala 3: replace "JFXApp" with "JFXApp3"

--- src/proscalafx/ch10/fxml/FXMLAdoptionForm.scala
+++ src/proscalafx/ch10/fxml/FXMLAdoptionForm.scala
@@ -1,33 +1,38 @@
 package proscalafx.ch10.fxml
 
-import java.io.IOException
-import javafx.{fxml => jfxf}
-import javafx.{scene => jfxs}
+import javafx.{fxml => jfxf, scene => jfxs}
 import scalafx.Includes._
-import scalafx.application.JFXApp
-import scalafx.application.JFXApp.PrimaryStage
+import scalafx.application.JFXApp3
+import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.scene.Scene
+
+import java.io.IOException
 
 /** Example of using FXMLLoader from ScalaFX.
   *
   * @author Jarek Sacha
   */
-object FXMLAdoptionForm extends JFXApp {
+object FXMLAdoptionForm extends JFXApp3 {
 
-  val resource = getClass.getResource("AdoptionForm.fxml")
-  if (resource == null) {
-    throw new IOException("Cannot load resource: AdoptionForm.fxml")
+  def start(): Unit = {
+
+
+    val resource = getClass.getResource("AdoptionForm.fxml")
+    if (resource == null) {
+      throw new IOException("Cannot load resource: AdoptionForm.fxml")
+    }
+
+    // NOTE: ScalaFX doe not yet provide a wrapper fro FXMLLoader (2012.11.12)
+    // We load here FXML content using JavaFX directly.
+    // It is important to provide type for the element loaded,
+    // though it can be a generic, here use `javafx.scene.parent`.
+    val root: jfxs.Parent = jfxf.FXMLLoader.load(resource)
+
+    stage = new PrimaryStage() {
+      title = "FXML GridPane Demo"
+      scene = new Scene(root)
+    }
   }
 
-  // NOTE: ScalaFX doe not yet provide a wrapper fro FXMLLoader (2012.11.12)
-  // We load here FXML content using JavaFX directly.
-  // It is important to provide type for the element loaded,
-  // though it can be a generic, here use `javafx.scene.parent`.
-  val root: jfxs.Parent = jfxf.FXMLLoader.load(resource)
-
-  stage = new PrimaryStage() {
-    title = "FXML GridPane Demo"
-    scene = new Scene(root)
-  }
 
 }
